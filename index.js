5359f18f3d5fcff8236bc42d4ef9d6e832365970
mitchelleokemwa <mitchelleokemwa@gmail.com>
2022-11-29 09:02:59 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(a,b){
+    return a + b;
+}
+add (12,7)
+
+function subtract(a,b){
+    return a - b;
+}
+subtract (12,7)
+
+function multiply(a,b){
+    return a * b;
+}
+multiply (12,7)
+
+function divide(a,b){
+    return a/b;
+}
+divide (12,4)
+
+function increment(n){
+    return ++n;
+}
+increment (5);
+
+function decrement(n){
+    return --n;
+}
+increment (9);
+
+function makeInt(n){
+   return  parseInt(n,10);
+    
+}
+function preserveDecimal(n){
+    return parseFloat(n);
+}
